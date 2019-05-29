419db7e571785f0bf99a1c5dc97252004fca9b83
hemapriyamaheswaran <45891957+hemapriyamaheswaran@users.noreply.github.com>
2019-05-29 10:24:10 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-terraform output kube_config > ~/.kube/aksconfig
-export KUBECONFIG=~/.kube/aksconfig
-kubectl apply -f deployment.yml
-kubectl apply -f https://k8s.io/examples/pods/storage/pv-volume.yaml
-kubectl apply -f https://k8s.io/examples/pods/storage/pv-claim.yaml
-
